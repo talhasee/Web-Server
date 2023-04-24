0968f1dd1f59d5061aa3f30d9c3773947e7474b4
Chetan <chetan.kvvk@gmail.com>
2023-04-24 23:46:43 +0530

    final changes

--- server.py
+++ server.py
@@ -53,15 +53,43 @@ def get_graduate_info():
             if row[1] == roll_number and row[0] == graduate_name.lower() and row[2] == dob:
                 found = True
                 print(row[4])
-                if row[4] == hashed_password:
+                print(hashed_password)
+                if row[4].strip() == hashed_password:
                     #Generate public and private key pair
-                    private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
-                    public_key = private_key.public_key()
+                    # private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+                    # public_key = private_key.public_key()
+
+                    #Generate public and private key pair of REGISTRAR
+                    private_key_registrar = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+                    public_key_registrar = private_key_registrar.public_key()
+
+                    #Generate public and private key pair of DIRECTOR
+                    private_key_director = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+                    public_key_director = private_key_director.public_key()
+
+
+                    print(row[4] == hashed_password)
+                    #Generate Degree_Certificate and Grade Card 
+                    signature_registrar,signature_director, pdf_hash= generate_certificate(graduate_name, roll_number, private_key_registrar, private_key_director)
+                    signature_registrar_grade,signature_director_grade, pdf_hash_grade= generate_grade_card(graduate_name, roll_number, private_key_registrar, private_key_director)
+
 
                     #Generate Degree_Certificate and Grade Card
-                    generate_certificate(graduate_name, roll_number, private_key)
+                    # generate_certificate(graduate_name, roll_number, private_key)
+                    # Verify the signature of Registrar
+                    is_verified_registrar = verify_signature(public_key_registrar, signature_registrar, pdf_hash)
+                    # Verify the signature of Director
+                    is_verified_director = verify_signature(public_key_director, signature_director, pdf_hash)
 
-                    return redirect(url_for('download_pdf', filename=f"{graduate_name}.pdf"))
+
+                    if is_verified_registrar and is_verified_director:
+                        print("Document is digitally signed by both Registrar and Director")
+                        return render_template('download_files.html', degree_name=f"{graduate_name}.pdf", grade_name=f"{graduate_name}_gradecard.pdf")
+
+                    else:
+                        print("Document signature is invalid")
+                        return "Document signature is invalid"
+
                 else:
                     return "Authentication Failed: Incorrect Password"
         if not found:
@@ -87,6 +115,34 @@ def get_ntp_time():
         return time.ctime(response)
     
 
+# Function to digitally sign a document using a private key
+def sign_document(private_key, document):
+    signature = private_key.sign(
+        document,
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+    return signature
+
+# Function to verify a digital signature using a public key
+def verify_signature(public_key, signature, document):
+    try:
+        public_key.verify(
+            signature,
+            document,
+            padding.PSS(
+                mgf=padding.MGF1(hashes.SHA256()),
+                salt_length=padding.PSS.MAX_LENGTH
+            ),
+            hashes.SHA256()
+        )
+        return True
+    except:
+        return False
+
 # Define the directory where the signed documents will be stored
 DOCUMENT_DIR = 'documents/'
 
@@ -108,7 +164,7 @@ if not os.path.exists(ROOT_CERT_FILE) or not os.path.exists(ROOT_KEY_FILE):
         f.write(root_cert.public_bytes(encoding=serialization.Encoding.PEM))
 
 
-def generate_certificate(name, roll_number, private_key):
+def generate_certificate(name, roll_number, private_key_registrar, private_key_director):
     # Create a new PDF canvas with the pre-made template
     doc_name = name + ".pdf"
     pdf_canvas = canvas.Canvas(doc_name)
@@ -133,7 +189,16 @@ def generate_certificate(name, roll_number, private_key):
     pdf_hash = hashlib.sha256(pdf_data).digest()
 
     # Sign the PDF hash with the private key
-    signature = private_key.sign(
+    signature = private_key_registrar.sign(
+        pdf_hash,
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+
+    director_signature = private_key_director.sign(
         pdf_hash,
         padding.PSS(
             mgf=padding.MGF1(hashes.SHA256()),
@@ -143,13 +208,17 @@ def generate_certificate(name, roll_number, private_key):
     )
 
     # Add the signature to the PDF
-    signature_text = f"Signature: {signature.hex()}"
+    signature_text = f"Registrar: {signature.hex()} Director: {director_signature.hex()}"
+
     max_width = 400  # set the maximum width of the text
     lines = textwrap.wrap(signature_text, width=50)  # split the text into lines of 50 characters
     for i, line in enumerate(lines):
         y = signature_y - i * 15  # change y coordinate for each line to simulate wrapping
         pdf_canvas.drawString(signature_x, y, line)
+
     pdf_canvas.save()
+
+
 
     # Add watermark to the PDF
     watermark_text = f"Issued to {roll_number} on {timestamp_str} by ABC University"
@@ -183,6 +252,101 @@ def generate_certificate(name, roll_number, private_key):
 
     print(f"Certificate for {name} ({roll_number}) has been generated and saved as {doc_name}.")
 
+    return signature, director_signature, pdf_hash
+
+
+def generate_grade_card(name, roll_number, private_key_registrar, private_key_director):
+    # Create a new PDF canvas with the pre-made template
+    doc_name = name + "_gradecard.pdf"
+    pdf_canvas = canvas.Canvas(doc_name)
+    pdf_canvas.setPageSize((595.27, 841.89))
+
+    # Define the location of the text fields on the PDF
+    name_x, name_y = 100, 750
+    roll_number_x, roll_number_y = 100, 700
+    timestamp_x, timestamp_y = 100, 650
+    signature_x, signature_y = 100, 550  # Change y coordinate for signature
+    grade_x, grade_y = 100, 550  # Change y coordinate for signature
+
+    # Add the graduate's information to the PDF
+    pdf_canvas.drawString(name_x, name_y, f"Name: {name.title()}")
+    pdf_canvas.drawString(roll_number_x, roll_number_y, f"Roll Number: {roll_number}")
+    pdf_canvas.drawString(grade_x, grade_y, f"Grade: X")
+
+
+    # Add the timestamp to the PDF
+    timestamp_str = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3] + " UTC"
+    pdf_canvas.drawString(timestamp_x, timestamp_y, f"Timestamp: {timestamp_str}")
+
+    # Hash the PDF data
+    pdf_data = pdf_canvas.getpdfdata()
+    pdf_hash = hashlib.sha256(pdf_data).digest()
+
+    # Sign the PDF hash with the private key
+    signature = private_key_registrar.sign(
+        pdf_hash,
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+
+    director_signature = private_key_director.sign(
+        pdf_hash,
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+
+    # Add the signature to the PDF
+    signature_text = f"Registrar: {signature.hex()} Director: {director_signature.hex()}"
+
+    max_width = 400  # set the maximum width of the text
+    lines = textwrap.wrap(signature_text, width=50)  # split the text into lines of 50 characters
+    for i, line in enumerate(lines):
+        y = signature_y - i * 15  # change y coordinate for each line to simulate wrapping
+        pdf_canvas.drawString(signature_x, y, line)
+
+    pdf_canvas.save()
+
+
+
+    # Add watermark to the PDF
+    watermark_text = f"Issued to {roll_number} on {timestamp_str} by ABC University"
+    watermark_file = "watermark.pdf"
+    watermark_canvas = canvas.Canvas(watermark_file, pagesize=letter)
+    watermark_canvas.setFont('Helvetica-Bold', 15)
+    watermark_canvas.setFillColorRGB(0.5, 0.5, 0.5, 0.2)
+    watermark_canvas.rotate(45)
+    text_width = watermark_canvas.stringWidth(watermark_text)
+
+    x = -6.5 * inch
+    y = 0.5 * inch
+    while x < 8.5 * inch:
+        watermark_canvas.drawString(x, y, watermark_text)
+        x += text_width + 20
+
+    watermark_canvas.save()
+
+
+
+    # Merge the PDF with the watermark
+    input_pdf = PdfFileReader(open(doc_name, "rb"))
+    watermark_pdf = PdfFileReader(open(watermark_file, "rb"))
+    output = PdfFileWriter()
+    for i in range(input_pdf.getNumPages()):
+        page = input_pdf.getPage(i)
+        page.mergePage(watermark_pdf.getPage(0))
+        output.addPage(page)
+    with open(doc_name, "wb") as outputStream:
+        output.write(outputStream)
+
+    print(f"Certificate for {name} ({roll_number}) has been generated and saved as {doc_name}.")
+
+    return signature, director_signature, pdf_hash
 
 if __name__ == '__main__':
     app.run()
